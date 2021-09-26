a0c24acdfcac8ae667f0b089461b1bf5a3d80712
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-09-26 07:02:49 +0600

    Optimize

--- mkpac.py
+++ mkpac.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections.abc
 import csv
 import enum
+import itertools
 import json
 import logging
 import sys
@@ -26,28 +28,73 @@ class Column(enum.IntEnum):
     URLS = 2
 
 
+def parse_domain(domain):
+    if domain.startswith('*.'):
+        domain = domain[2:]
+
+    if domain.startswith('.'):
+        domain = domain[1:]
+
+    return tuple(part for part in reversed(domain.split('.')) if part)
+
+
+def unparse_domain(domain):
+    if len(domain) == 1:
+        return f'.{domain[0]}'
+
+    return '.'.join(reversed(domain))
+
+
+def parent_domains(parsed_domain):
+    for level in range(1, len(parsed_domain)):
+        yield parsed_domain[:level]
+
+
+INDENT = '\t'
+
+
+HEADER = f'''
+function DnsDomainInList(domain, domainList) {{
+{INDENT}for (var i = 0; i < domainList.length; i++) {{
+{INDENT * 2}if (dnsDomainIs(domain, domainList[i])) return true;
+{INDENT}}}
+{INDENT}return false;
+}}
+'''
+
+
 def run(dump_csv, output, proxy, nxdomain):
     if nxdomain:
-        nx_domains = set(line.strip() for line in nxdomain)
+        nx_domains = set(parse_domain(line.strip()) for line in nxdomain)
     else:
         nx_domains = set()
 
-    reader = csv.reader(dump_csv, delimiter=';')
-    next(reader)  # "Updated on ..."
-
     domains = set()
+
+    def add_domain(domain):
+        parsed_domain = parse_domain(domain)
+
+        if not parsed_domain:
+            return
+
+        if parsed_domain in nx_domains:
+            return
+
+        if any((parent_domain in nx_domains) for parent_domain in parent_domains(parsed_domain)):
+            return
+
+        if all(part.isdecimal() for part in parsed_domain):
+            return
+
+        domains.add(parsed_domain)
+
+    reader = csv.reader(dump_csv, delimiter=';')
+    next(reader)  # Skip "Updated on ..."
 
     for line_number, row in enumerate(reader, start=2):
         for domain in iter_field(row[Column.DOMAINS]):
-            if domain.startswith('*.'):
-                domain = domain[2:]
-
-            if domain.startswith('.'):
-                domain = domain[1:]
-
             if domain:
-                if domain not in nx_domains:
-                    domains.add(domain)
+                add_domain(domain)
             else:
                 logging.warning("Empty domain name in line %d", line_number)
 
@@ -55,8 +102,7 @@ def run(dump_csv, output, proxy, nxdomain):
             try:
                 domain = urllib.parse.urlsplit(url, scheme='http').hostname
                 if domain:
-                    if domain not in nx_domains:
-                        domains.add(domain)
+                    add_domain(domain)
                 else:
                     logging.warning("Can't find hostname in %r URL, line %d", url, line_number)
 
@@ -65,21 +111,61 @@ def run(dump_csv, output, proxy, nxdomain):
 
     logging.info('Total domains: %d', len(domains))
 
-    pac = f'''
-function FindProxyForURL(url, host) {{
-    const PROXY = {json.dumps(proxy)};
-    const PROXY_DOMAINS = {json.dumps(list(domains), indent=4)};
+    def gen(parent_domain, domains):
+        groups = {}
 
-    for (var i = 0; i < PROXY_DOMAINS.length; i++) {{
-        if (dnsDomainIs(host, PROXY_DOMAINS[i])) {{
-            return PROXY;
-        }}
-    }}
+        for k, g in itertools.groupby(domains, key=lambda domain: domain[:len(parent_domain) + 1]):
+            if k == parent_domain:
+                return k
 
-    return "DIRECT";
-}}'''
+            groups[k] = gen(k, g)
 
-    print(pac.strip(), file=output)
+        if len(groups) == 1:
+            return next(iter(groups.values()))
+
+        return groups
+
+    print(HEADER, file=output)
+
+    func_counter = 0
+
+    def gencode(groups):
+        nonlocal func_counter
+        func_name = f'Match{func_counter}'
+        func_counter += 1
+
+        simple_yes = []
+        call_other = []
+
+        for k, g in groups.items():
+            if isinstance(g, collections.abc.Mapping):
+                call_other.append((unparse_domain(k), gencode(g)))
+            else:
+                simple_yes.append(unparse_domain(k))
+
+        print(f'function {func_name}(domain) {{', file=output)
+
+        if simple_yes:
+            print(f'{INDENT}const SIMPLE_YES =', file=output, end='')
+            json.dump(simple_yes, output, indent=INDENT * 2)
+            print(';', file=output)
+            print(f'{INDENT}if (DnsDomainInList(domain, SIMPLE_YES)) return true;', file=output)
+
+        for domain, func in call_other:
+            print(f'{INDENT}if (dnsDomainIs(domain, {json.dumps(domain)})) return {func}(domain);', file=output)
+
+        print(f'{INDENT}return false;', file=output)
+        print(f'}}', file=output)
+        print('', file=output)
+        return func_name
+
+    main_match = gencode(gen(tuple(), sorted(domains)))
+
+    print('function FindProxyForURL(url, host) {', file=output)
+    print(f'{INDENT}if ({main_match}(host)) return {json.dumps(proxy)};', file=output)
+    print(f'{INDENT}return "DIRECT";', file=output)
+    print('}', file=output)
+
     output.close()
 
 
